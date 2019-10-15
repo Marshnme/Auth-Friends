e91aa3ae9a1711110f2dcdfd9348df3d32197f08
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-10-15 17:47:30 -0400

    working on looping through data to display

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -4,18 +4,29 @@ import {axiosWithAuth} from "../utils/axiosWithAuth";
 const FriendsList = () => {
     const [friends, setFriends] = useState({})
 
-
+    useEffect(()=> {
+        getFriends()
+    },[])
 
     const getFriends = () =>{
+        console.log("hello?",)
         axiosWithAuth()
             .get("/api/friends")
             .then(res => {
                 console.log(res)
                 setFriends(res.data)
+                console.log("FRIENDS",friends)
             })
     }
     return(
-        <p>hi</p>
+        <div>
+            {/* {friends.map(friend => (
+                <div>
+                    <p>{friend.name}</p>
+                    <p>{friend.age}</p>
+                </div>
+            ))} */}
+        </div>
     );
 };
 
